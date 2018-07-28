befbb7a1ecd738eeaa136a46cb02f00e6f37d568
sonicakes <soniaparker1207@gmail.com>
2018-07-28 16:57:50 +1000

    es6 exercises

--- rest-spread.js
+++ rest-spread.js
@@ -17,3 +17,29 @@ function smallestValue(arr){
 }
 //test
 console.log(smallestValue([3,4,5,3,1]));
+console.log(smallestValue([4,2]));
+
+/* 
+Write a function called placeInMiddle which accepts two parameters, an array and another array.
+ This function should return the first array with all of the values in the second array placed in the middle of the first array.
+
+Examples:
+    placeInMiddle([1,2,6,7],[3,4,5]) // [1,2,3,4,5,6,7]
+    placeInMiddle([1],[3,4,5]) // [3,4,5,1]
+    placeInMiddle([1,6],[2,3,4,5]) // [1,2,3,4,5,6]
+    placeInMiddle([],[2,3,4,5]) // [2,3,4,5]
+*/
+
+function placeInMiddle(arr, vals){
+    //find middle of the array
+    let middleOfArray = arr.length/2;
+    console.log("length of array is " + middleOfArray);
+    //now we know the first parameter to pass into splice() method, this is our middleOfArray
+     arr.splice(middleOfArray, 0, ...vals);
+     return arr;
+}
+//testing
+console.log(placeInMiddle([1,2,6,7],[3,4,5])); // [1,2,3,4,5,6,7]
+console.log(placeInMiddle([1],[3,4,5])); // [3,4,5,1]
+console.log(placeInMiddle([1,6],[2,3,4,5])); // [1,2,3,4,5,6]
+console.log(placeInMiddle([],[2,3,4,5])); // [2,3,4,5]
